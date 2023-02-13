f4ef52a0861d6cab8508206ed232bae62bbec466
Joe Dravarol <joedravarol@gmail.com>
2023-02-13 14:16:24 +0000

    feat: desktop hero

--- src/features/Hero.tsx
+++ src/features/Hero.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+import desktopHeroBgSm from '@/assets/homepage/hero-bg-desktop.jpg';
+import desktopHeroBgMd from '@/assets/homepage/hero-bg-desktop@2x.jpg';
 import mobileHeroBgSm from '@/assets/homepage/hero-bg-mobile.jpg';
 import mobileHeroBgMd from '@/assets/homepage/hero-bg-mobile@2x.jpg';
 import tabletHeroBgSm from '@/assets/homepage/hero-bg-tablet.jpg';
@@ -25,16 +27,36 @@ const StyledContainer = styled(Box)(({ theme }) => ({
     maxWidth: 689,
     marginInline: 'auto',
   },
+  [theme.breakpoints.up('desktop')]: {
+    margin: 'revert',
+    marginTop: 65,
+    marginLeft: 165,
+    textAlign: 'revert',
+  },
 }));
 
 const StyledButton = styled(Button)(() => ({
   width: 245,
 }));
 
+const StyledImage = styled(Image)(({ theme }) => ({
+  pointerEvents: 'none',
+  position: 'absolute',
+  width: '100%',
+  objectFit: 'cover',
+  objectPosition: 'top',
+  zIndex: -1,
+
+  [theme.breakpoints.up('desktop')]: {
+    height: '100%',
+    objectPosition: 'revert',
+  },
+}));
+
 const Hero: React.FC = () => {
   return (
     <Box
-      height={{ mobile: 748, tablet: 992 }}
+      height={{ mobile: 748, tablet: 992, desktop: 820 }}
       component="header"
       bgcolor="primary.main"
       position="relative"
@@ -42,21 +64,12 @@ const Hero: React.FC = () => {
       zIndex={1}
     >
       <picture>
+        <source srcSet={desktopHeroBgMd.src} media="(min-width: 1920px)" />
+        <source srcSet={desktopHeroBgSm.src} media="(min-width: 1440px)" />
         <source srcSet={tabletHeroBgMd.src} media="(min-width: 900px)" />
         <source srcSet={tabletHeroBgSm.src} media="(min-width: 768px)" />
         <source srcSet={mobileHeroBgMd.src} media="(min-width: 400px)" />
-        <Image
-          style={{
-            pointerEvents: 'none',
-            position: 'absolute',
-            width: '100%',
-            objectFit: 'cover',
-            objectPosition: 'top',
-            zIndex: -1,
-          }}
-          src={mobileHeroBgSm}
-          alt="test"
-        />
+        <StyledImage src={mobileHeroBgSm} alt="Plate of shrimp noodles" />
       </picture>
 
       <StyledContainer component="section">
@@ -67,22 +80,22 @@ const Hero: React.FC = () => {
         <Typography
           variant="h1"
           component="h2"
-          fontSize={{ mobile: '2rem', tablet: '3rem' }}
-          lineHeight={{ mobile: '2.5rem', tablet: '4rem' }}
-          letterSpacing={{ mobile: '-0.025rem', tablet: '-0.038rem' }}
-          mt={{ mobile: 4.5, tablet: 4.75 }}
-          mb={{ mobile: 2.625, tablet: 2.5 }}
+          fontSize={{ mobile: '2rem', tablet: '3rem', desktop: '5rem' }}
+          lineHeight={{ mobile: '2.5rem', tablet: '4rem', desktop: '5rem' }}
+          letterSpacing={{ mobile: '-0.025rem', tablet: '-0.038rem', desktop: '-0.063rem' }}
+          mt={{ mobile: 4.5, tablet: 4.75, desktop: 19.125 }}
+          mb={{ mobile: 2.625, tablet: 2.5, desktop: 1.25 }}
         >
           Exquisite dining <Box component="span" display="block">since 1989</Box>
         </Typography>
 
         <Typography
-          sx={{ marginInline: { tablet: 'auto' } }}
+          sx={{ marginInline: { tablet: 'auto', desktop: 'revert' } }}
           variant="body2"
-          maxWidth={{ tablet: 573 }}
+          maxWidth={{ tablet: 573, desktop: 445 }}
           fontSize={{ tablet: '1.25rem' }}
           lineHeight={{ tablet: '1.875rem' }}
-          mb={{ mobile: 6.325, tablet: 6.5 }}
+          mb={{ mobile: 6.325, tablet: 6.5, desktop: 5 }}
         >
           Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of
           our farmhouse.
