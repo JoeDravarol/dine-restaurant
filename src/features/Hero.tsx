0e51ec3ad6cfd6457b23ce7a3881367d5d79b1fc
Joe Dravarol <joedravarol@gmail.com>
2023-02-13 13:36:42 +0000

    feat: tablet hero

--- src/features/Hero.tsx
+++ src/features/Hero.tsx
@@ -7,6 +7,8 @@ import React from 'react';
 
 import mobileHeroBgSm from '@/assets/homepage/hero-bg-mobile.jpg';
 import mobileHeroBgMd from '@/assets/homepage/hero-bg-mobile@2x.jpg';
+import tabletHeroBgSm from '@/assets/homepage/hero-bg-tablet.jpg';
+import tabletHeroBgMd from '@/assets/homepage/hero-bg-tablet@2x.jpg';
 import Logo from '@/assets/icons/logo.svg';
 import Button from '@/components/Button';
 
@@ -17,6 +19,12 @@ const StyledContainer = styled(Box)(({ theme }) => ({
   color: theme.palette.common.white,
   fill: 'white',
   textAlign: 'center',
+
+  [theme.breakpoints.up('tablet')]: {
+    marginTop: 'clamp(374px, 40%, 600px)',
+    maxWidth: 689,
+    marginInline: 'auto',
+  },
 }));
 
 const StyledButton = styled(Button)(() => ({
@@ -25,8 +33,17 @@ const StyledButton = styled(Button)(() => ({
 
 const Hero: React.FC = () => {
   return (
-    <Box height={748} component="header" position="relative" overflow="hidden">
+    <Box
+      height={{ mobile: 748, tablet: 992 }}
+      component="header"
+      bgcolor="primary.main"
+      position="relative"
+      overflow="hidden"
+      zIndex={1}
+    >
       <picture>
+        <source srcSet={tabletHeroBgMd.src} media="(min-width: 900px)" />
+        <source srcSet={tabletHeroBgSm.src} media="(min-width: 768px)" />
         <source srcSet={mobileHeroBgMd.src} media="(min-width: 400px)" />
         <Image
           style={{
@@ -50,16 +67,23 @@ const Hero: React.FC = () => {
         <Typography
           variant="h1"
           component="h2"
-          fontSize="2rem"
-          lineHeight="2.5rem"
-          letterSpacing="-0.025rem"
-          mt={4.5}
-          mb={2.625}
+          fontSize={{ mobile: '2rem', tablet: '3rem' }}
+          lineHeight={{ mobile: '2.5rem', tablet: '4rem' }}
+          letterSpacing={{ mobile: '-0.025rem', tablet: '-0.038rem' }}
+          mt={{ mobile: 4.5, tablet: 4.75 }}
+          mb={{ mobile: 2.625, tablet: 2.5 }}
         >
           Exquisite dining <Box component="span" display="block">since 1989</Box>
         </Typography>
 
-        <Typography variant="body2" mb={6.325}>
+        <Typography
+          sx={{ marginInline: { tablet: 'auto' } }}
+          variant="body2"
+          maxWidth={{ tablet: 573 }}
+          fontSize={{ tablet: '1.25rem' }}
+          lineHeight={{ tablet: '1.875rem' }}
+          mb={{ mobile: 6.325, tablet: 6.5 }}
+        >
           Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of
           our farmhouse.
         </Typography>
