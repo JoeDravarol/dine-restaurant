a71023781753859c95fe454b332f59d976ae683b
Joe Dravarol <joedravarol@gmail.com>
2023-02-18 00:04:26 +0000

    feat: tablet CTA & minor refactor

--- src/features/CTA.tsx
+++ src/features/CTA.tsx
@@ -2,11 +2,30 @@ import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 import mobileBgSm from '@/assets/homepage/ready-bg-mobile.jpg';
 import mobileBgMd from '@/assets/homepage/ready-bg-mobile@2x.jpg';
+import tabletBgSm from '@/assets/homepage/ready-bg-tablet.jpg';
+import tabletBgMd from '@/assets/homepage/ready-bg-tablet@2x.jpg';
 import Button from '@/components/Button';
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  paddingInline: theme.spacing(3),
+  paddingTop: 84,
+  paddingBottom: 80,
+  textAlign: 'center',
+  color: theme.palette.common.white,
+  position: 'relative',
+  overflow: 'hidden',
+
+  [theme.breakpoints.up('tablet')]: {
+    paddingInline: 66,
+    paddingTop: 72,
+    paddingBottom: 64,
+  },
+}));
 
 const StyledImage = styled(Image)(() => ({
   width: '100%',
@@ -21,31 +40,26 @@ const StyledImage = styled(Image)(() => ({
 
 const CTA: React.FC = () => {
   return (
-    <Box
-      px={{ mobile: 3 }}
-      pt={{ mobile: 10.5 }}
-      pb={{ mobile: 10 }}
-      textAlign={{ mobile: 'center' }}
-      color="common.white"
-      position="relative"
-    >
+    <StyledBox component="section">
       <picture>
+        <source srcSet={tabletBgMd.src} media="(min-width: 900px)" />
+        <source srcSet={tabletBgSm.src} media="(min-width: 768px)" />
         <source srcSet={mobileBgMd.src} media="(min-width: 400px)" />
         <StyledImage src={mobileBgSm} alt="Plate of toast background image" />
       </picture>
 
       <Typography
         variant="h2"
-        fontSize={{ mobile: '2rem' }}
-        lineHeight={{ mobile: '2.5rem' }}
-        letterSpacing={{ mobile: '-0.025rem' }}
-        mb={{ mobile: 2.5 }}
+        fontSize={{ mobile: '2rem', tablet: '3rem' }}
+        lineHeight={{ mobile: '2.5rem', tablet: '3rem' }}
+        letterSpacing={{ mobile: '-0.025rem', tablet: '-0.031rem' }}
+        mb={{ mobile: 2.5, tablet: 3 }}
       >
         Ready to make a reservations?
       </Typography>
 
-      <Button sx={{ width: 245 }}>Book a table</Button>
-    </Box>
+      <Button sx={{ width: 245 }} component={Link} href="/booking">Book a table</Button>
+    </StyledBox>
   );
 };
 
